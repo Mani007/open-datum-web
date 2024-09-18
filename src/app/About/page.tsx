d7593ab1f68f90024e6041976d87a3ea3f11118e
Mohnish <mohnish.a007@gmail.com>
2024-09-18 23:27:11 +0100

    next Image working fine

--- src/app/About/page.tsx
+++ src/app/About/page.tsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import Image from "next/image";
 
 function About() {
   return (
-    <div>About</div>
+   
+    <>
+     <div>About</div>
+     <Image
+          className="dark:invert"
+          src="https://nextjs.org/icons/next.svg"
+          alt="Next.js logo"
+          width={180}
+          height={38}
+          priority
+        />
+    </>
   )
 }
 
