4f3731f35e0304f91304fb7d633695a7f7c46c4c
Mohnish <mohnish.a007@gmail.com>
2024-09-19 10:37:38 +0100

    Project Roadmap - check the width property in flexbox

--- src/app/Roadmap/page.tsx
+++ src/app/Roadmap/page.tsx
@@ -3,14 +3,14 @@ import React from 'react'
 function Roadmap() {
   return (
     <>
-    <section className="items-center  justify-center">
-        <div className="mb-20 mt-20 text-gray-800 text-center"> 
-          <h2 className="text-3xl font-bold mb-12">Project Road-Map</h2>
+    <section className="items-center bg-[#A7E6F1]  justify-center">
+        <div className="mb-5 mt-5 text-gray-800 text-center"> 
+          <h2 className="text-3xl font-bold mb-6">Project Road-Map</h2>
         </div>
         <div className="flex items-center justify-center py-10">
-  <div className="max-w-screen-sm lg:max-w-screen-md xl:max-w-screen-lg px-20 mx-auto">
+  <div className="max-w-screen-sm  lg:max-w-screen-md xl:max-w-screen-lg px-20 mx-auto">
     <ol className="relative border-l-4 border-indigo-600 leading-loose">
-      <li className="mb-10 ml-6 sm:w-[100px]">
+      <li className="mb-10 ml-6 ">
         <div
              className="absolute w-4 h-4 bg-green-500 border-4 border-indigo-600 rounded-full -left-[0.6rem]"
              ></div>
@@ -21,9 +21,9 @@ function Roadmap() {
            Leveraging power of Artificial intelligence in prediction of degradation of microplastics. 
         </p>
       </li>
-      <li className="mb-10 ml-6 sm:w-[100px]">
+      <li className="mb-10 ml-6 ">
         <div
-             className="absolute w-4 h-4 bg-white border-4 border-indigo-600 rounded-full -left-[0.6rem]"
+             className="absolute w-4 h-4 bg-green-500 border-4 border-indigo-600 rounded-full -left-[0.6rem]"
              ></div>
         <p className="absolute -left-[7rem] p-0 m-0 font-bold">May 2022</p>
         <p className="font-bold text-lg mb-1">
@@ -34,23 +34,23 @@ function Roadmap() {
           Making data preprocessing and trying out different time series analysis and prediction algorithms such as ARIMA, SARIMA etc.  
         </p>
       </li>
-      <li className="mb-10 ml-6 sm:w-[100px]">
+      <li className="mb-10 ml-6 ">
         <div
-             className="absolute w-4 h-4 bg-white border-4 border-indigo-600 rounded-full -left-[0.6rem]"
+             className="absolute w-4 h-4 bg-green-500 border-4 border-indigo-600 rounded-full -left-[0.6rem]"
              ></div>
         <p className="absolute -left-[7rem] p-0 m-0 font-bold">
-          June 2022
+        Nov 2022
           
         </p>
         <p className="font-bold text-lg mb-1">OpenDATUM Origin </p>
        
         <p>
-           Creating OpenDatum, the platform to accelerate the research of microplatic degradation and toxicology. 
+           Creating OpenDatum, the platform to accelerate the research of microplastics degradation and toxicology. 
         </p>
       </li>
-      <li className="mb-10 ml-6 sm:w-[100px]">
+      <li className="mb-10 ml-6 ">
         <div
-             className="absolute w-4 h-4 bg-white border-4 border-indigo-600 rounded-full -left-[0.6rem]"
+             className="absolute w-4 h-4 bg-green-500 border-4 border-indigo-600 rounded-full -left-[0.6rem]"
              ></div>
         <p className="absolute -left-[7rem] p-0 m-0 font-bold">June 2023</p>
         <p className="font-bold text-lg mb-1">OpenDatum </p>
@@ -59,23 +59,23 @@ function Roadmap() {
            Platform BETA launch with minium important features. Also include our AI models. 
         </p>
       </li>
-      <li className="mb-10 ml-6 sm:w-[100px]">
+      <li className="mb-10 ml-6 ">
         <div
              className="absolute w-4 h-4 bg-white border-4 border-indigo-600 rounded-full -left-[0.6rem]"
              ></div>
-        <p className="absolute -left-[9rem] p-0 m-0 font-bold">December 2024</p>
+        <p className="absolute -left-[9rem] p-0 m-0 font-bold">Dec 2024</p>
         <p className="font-bold text-lg mb-1">OpenDatum </p>
        
         <p>
-           Developers API and documentations released under MIT and other licenses
+           Release and open-source developers API and documentations under MIT and other licenses
         </p>
       </li>
-      <li className="mb-10 ml-6 sm:w-[100px]">
+      <li className="mb-10 ml-6 ">
         <div
              className="absolute w-4 h-4 bg-white border-4 border-indigo-600 rounded-full -left-[0.6rem]"
              ></div>
         <p className="absolute -left-[9rem] p-0 m-0 font-bold">
-          June 2024
+          June 2025
         </p>
         <p className="font-bold text-lg mb-1">
           OpenDATUM Globally
@@ -85,11 +85,11 @@ function Roadmap() {
           The suite of tools to be launched in the global scale with realtime data processing and inference capability. 
         </p>
       </li>
-      <li className="mb-10 ml-6 sm:w-[100px]">
+      <li className="mb-10 ml-6 ">
         <div
              className="absolute w-4 h-4 bg-white border-4 border-indigo-600 rounded-full -left-[0.6rem]"
              ></div>
-        <p className="absolute -left-[9rem] p-0 m-0 font-bold">December 2024</p>
+        <p className="absolute -left-[9rem] p-0 m-0 font-bold">Dec 2025</p>
         <p className="font-bold text-lg mb-1">
           OpenDatum on Blockchains
         </p>
